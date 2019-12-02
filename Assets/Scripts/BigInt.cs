621a55a50a0b377cb58a7d0d0f19082efd9cf15c
Egor <kolchev-00@mail.ru>
2019-12-02 20:00:32 +1000

    added test branch(bigInt)

---
+++ Assets/Scripts/BigInt.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BigInt
+{
+    public static List<int> value = new List<int>();
+    public static void plus (BigInt x)
+    {
+        // сложение больших чисел
+        for (int i = 0; i < x.value.Count; i++)
+        {
+            // если прибавляемое число имет больше разрядов чем то, к которому прибавляем
+            if (i > value.Count)
+            {
+                // скидываем лишние разряды в то, к которому прибавляем
+                value.Add(x.value[i]);
+            }
+            else 
+            {
+                // складываем текущие разряды
+                value[i] += x.value[i];
+                //если выходим из размера разряда и прибавляем к следующему излишки
+                if (value[i] / 1000 > 0)
+                {
+                    //если разряда еще нет
+                    if (i + 1 > value.Count)
+                    {
+                        value.Add(value[i] / 1000);
+                    }
+                    else
+                    {
+                        value[i + 1] += value[i] / 1000;
+                    }
+                    value[i] = value[i] % 1000;
+                }
+            }
+           
+
+        }
+        
+    }
+
+}
---
+++ Assets/Scripts/testBigInt.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testBigInt : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        BigInt test1 = new BigInt();
+        BigInt test2 = new BigInt();
+        int x = 123;
+        test1.value.Add(x);
+        test1.value.Add(2 * x);
+        test1.plus(test2);
+        Debug.Log("test", test1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
