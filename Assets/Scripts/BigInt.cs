3f084a7453603103171fbc52a64bbef97eb2675a
Egor <kolchev-00@mail.ru>
2019-12-03 00:13:23 +1000

    fixed BigInt, added plus function, added some fancy stuff(cool number display, etc). PLS,HELP ME, I NEED SOMEBODY TO INTEGRATE THIS INTO UNITY NORMALLY

--- Assets/Scripts/BigInt.cs
+++ Assets/Scripts/BigInt.cs
@@ -4,41 +4,139 @@ using UnityEngine;
 
 public class BigInt
 {
-    public static List<int> value = new List<int>();
-    public static void plus (BigInt x)
+    private List<int> value = new List<int>();
+    //добавляет разряд со значением v
+    public void add(int v)
+    {
+        this.value.Add(v);
+    }
+    //возвращает количество разрядов
+    public int count()
+    {
+        return this.value.Count;
+    }
+    // возвращает значение указанного разряда
+    public int getCatNum(int category)
+    {
+        return this.value[category];
+    }
+    // превращает разряд внутри числа в его нормальное отображение
+    private string tranformToString(int x)
+    {
+        if (x < 0)
+        {
+            return "ERROR:can't tranform";
+        }
+        string s = "";
+        if (x > 99)
+        {
+            s += x;
+        }
+        else if (x > 9)
+        {
+            s += "0";
+            s += x;
+        }
+        else
+        {
+            s += "00";
+            s += x;
+        }
+        return s;
+    }
+    // крутое отображение чисел как Кирилл и хотел
+    public string showAsLetter()
+    {
+        int count = this.count();
+        if (count <= 0)
+        {
+            return "error";
+
+        }
+
+        string s = "";
+        s += this.value[count - 1];
+        if (count > 1)
+        {
+            s += "." + tranformToString(this.value[count - 2]);
+            switch (count)
+            {
+                case 2:
+                    break;
+                case 3:
+                    s += "M";
+                    break;
+                case 4:
+                    s += "B";
+                    break;
+                case 5:
+                    s += "T";
+                    break;
+                case 6:
+                    s += "Q";
+                    break;
+                default:
+                    s += "A LOT OF SHIT IF HAPPENING HERE";
+                    break;
+            }
+        }
+
+        return s;
+
+    }
+    // просто отображает все число
+    public string show()
+    {
+        if (this.count() > 0)
+        {
+            string s = this.value[this.count() - 1].ToString();
+            for (int i = this.count() - 2; i >= 0; i--)
+            {
+                s += tranformToString(this.value[i]);
+            }
+            return s;
+        }
+        else
+        {
+            return "error";
+        }
+
+    }
+    // сложение двух больших чисел
+    public void plus(BigInt x)
     {
         // сложение больших чисел
-        for (int i = 0; i < x.value.Count; i++)
+        for (int i = 0; i < x.count(); i++)
         {
             // если прибавляемое число имет больше разрядов чем то, к которому прибавляем
-            if (i > value.Count)
+            if (i >= this.value.Count)
             {
                 // скидываем лишние разряды в то, к которому прибавляем
-                value.Add(x.value[i]);
+                this.value.Add(x.getCatNum(i));
             }
-            else 
+            else
             {
                 // складываем текущие разряды
-                value[i] += x.value[i];
+                this.value[i] += x.getCatNum(i);
                 //если выходим из размера разряда и прибавляем к следующему излишки
-                if (value[i] / 1000 > 0)
+                if (this.value[i] / 1000 > 0)
                 {
                     //если разряда еще нет
-                    if (i + 1 > value.Count)
+                    if (i + 1 >= value.Count)
                     {
-                        value.Add(value[i] / 1000);
+                        this.value.Add(value[i] / 1000);
                     }
                     else
                     {
-                        value[i + 1] += value[i] / 1000;
+                        this.value[i + 1] += value[i] / 1000;
                     }
-                    value[i] = value[i] % 1000;
+                    this.value[i] = value[i] % 1000;
                 }
             }
-           
+
 
         }
-        
+
     }
 
 }
--- Assets/Scripts/testBigInt.cs
+++ Assets/Scripts/testBigInt.cs
@@ -7,13 +7,67 @@ public class testBigInt : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        BigInt test1 = new BigInt();
-        BigInt test2 = new BigInt();
-        int x = 123;
-        test1.value.Add(x);
-        test1.value.Add(2 * x);
-        test1.plus(test2);
-        Debug.Log("test", test1);
+        {
+            BigInt test1 = new BigInt();
+            BigInt test2 = new BigInt();
+            test1.add(1);
+            Console.WriteLine(test1.show());
+            test2.add(1);
+            test1.add(2);
+            test2.add(2);
+            test2.add(333);
+            test1.plus(test2);
+            Console.WriteLine(test1.show());
+            test2.add(1);
+            test2.add(2);
+            test2.add(3);
+            Console.WriteLine(test2.showAsLetter());
+            test2.add(1);
+            test2.add(1);
+            test2.add(1);
+            test2.add(1);
+            test2.add(1);
+            test2.add(1);
+            test2.add(1);
+            test2.add(1);
+            test2.add(1);
+            test2.add(1);
+            test2.add(1);
+            test2.add(1);
+            test2.add(1);
+            test2.add(1);
+            test2.add(1);
+            test2.add(1);
+            test2.add(1);
+            test2.add(1);
+            test2.add(1);
+            test2.add(1);
+            test2.add(1);
+            test2.add(1);
+            test2.add(1);
+            test2.add(1);
+            test2.add(1);
+            test2.add(1);
+            test2.add(1);
+            test2.add(1);
+            test2.add(1);
+            test2.add(1);
+            test2.add(1);
+            test2.add(1);
+            test2.add(1);
+            test2.add(1);
+            test2.add(1);
+            test2.add(1);
+            test2.add(1);
+            test2.add(1);
+            test2.add(1);
+            test2.add(1);
+            test2.add(1);
+            test2.add(1);
+            test2.plus(test2);
+            Console.WriteLine(test2.show());
+            Console.WriteLine(test2.showAsLetter());
+        }
     }
 
     // Update is called once per frame
